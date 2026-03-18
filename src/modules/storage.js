1d4d7349474851245276a18f7baab07bfbc1ab48
pakaimarkk <251377257+pakaimarkk-sketch@users.noreply.github.com>
2026-03-18 19:40:13 +0100

    add notes to localstorage

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,4 +1,6 @@
 const TASKS_KEY = "tasks";
+const NOTES_KEY = "notes";
+
 
 export function loadTasks() {
   return JSON.parse(localStorage.getItem(TASKS_KEY)) || [];
@@ -6,4 +8,12 @@ export function loadTasks() {
 
 export function saveTasks(tasks) {
   localStorage.setItem(TASKS_KEY, JSON.stringify(tasks));
+}
+
+export function loadNotes() {
+  return JSON.parse(localStorage.getItem(NOTES_KEY)) || [];
+}
+
+export function saveNotes(notes) {
+  localStorage.setItem(TASKS_KEY, JSON.stringify(notes));
 }
