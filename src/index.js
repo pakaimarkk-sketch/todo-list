3ad4975a39a2b13208d17e49cca4e39a4d7a1188
pakaimarkk <251377257+pakaimarkk-sketch@users.noreply.github.com>
2026-03-24 00:07:17 +0100

    add project buttons to init

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import { updateUI } from "./modules/screenController";
-import { sidebarBtns } from "./modules/screenController";
-import { showView } from "./modules/screenController";
+import { bindAddProjectButton } from "./modules/projects/projectController";
+import { bindProjectNavButtons, renderProjectButtons, } from "./modules/projects/projectNav";
+import { sidebarBtns, showView, } from "./modules/screenController";
 import "./styles.css";
 
 const init = () => {
     sidebarBtns();
-    showView("day")
+    showView("day");
+    bindAddProjectButton();
+    bindProjectNavButtons();
+    renderProjectButtons();
 };
 
 init();
