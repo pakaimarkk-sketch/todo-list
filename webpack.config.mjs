964c24fc1a80fff0bb91d84665b33f19b9ef6e7f
pakaimarkk <251377257+pakaimarkk-sketch@users.noreply.github.com>
2026-03-24 02:59:51 +0100

    Update app

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,36 +1,38 @@
 import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
-const isProduction = process.env.NODE_ENV === "production";
+export default (env, argv) => {
+  const isProduction = argv.mode === "production";
 
-export default {
-  mode: isProduction ? "production" : "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
-    clean: true,
-    publicPath: isProduction ? "/todo-list/" : "/",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        use: ["html-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
+  return {
+    mode: isProduction ? "production" : "development",
+    entry: "./src/index.js",
+    output: {
+      filename: "main.js",
+      path: path.resolve(import.meta.dirname, "dist"),
+      clean: true,
+      publicPath: isProduction ? "/todo-list/" : "/",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
     ],
-  },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.html$/i,
+          use: ["html-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+  };
 };
